fadcdc93a8054473b71247de28b1f2779b998c21
unknown <gibmac@outlook.com>
2022-08-15 16:38:32 -0600

    image attempt fix3

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -8,7 +8,7 @@ function ProductCard({name, picture, price}: {name: string, picture:string, pric
         <h1 className='z-10 absolute top-2 left-1/2 -translate-x-1/2 mx-auto bg-mainGold rounded-md text-center p-2'>{name}</h1>
       </div>
       <div className='bg-gray-300'>
-        <Image className='p-1'src={picture} />
+        <Image className='p-1 relative'src={picture} />
         <p className='text-right mr-2 text-lg font-bold'>${price}</p>
       </div>
     </section>
