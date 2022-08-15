afe9290a7809f43b36757ea1379da5cc4fccc200
unknown <gibmac@outlook.com>
2022-08-15 14:01:15 -0600

    image/img fix

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 function ProductCard({name, picture, price}: {name: string, picture:string, price: number}) {
   return (
@@ -7,7 +8,7 @@ function ProductCard({name, picture, price}: {name: string, picture:string, pric
         <h1 className='z-10 absolute top-2 left-1/2 -translate-x-1/2 mx-auto bg-mainGold rounded-md text-center p-2'>{name}</h1>
       </div>
       <div className='bg-gray-300'>
-        <img className='p-1'src={picture} />
+        <Image className='p-1'src={picture} />
         {<p className='text-right mr-2 text-lg font-bold'>${price}</p>}
       </div>
     </section>
